e36abc4311f86935b8da674ee075a649d5ef1a87
Philip DeFraties <philipjfry338@gmail.coom>
2020-09-09 16:03:52 -0600

    Add sadpath logic for user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,21 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to '/users/profile'
-      flash[:success] = "Welcome #{params[:name]}, you are now registered and logged in!"
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to "/users/#{@user.id}"
+      flash[:success] = "Welcome #{user_params["name"]}, you are now registered and logged in!"
     else
-      flash[:error] = 'User not created, all fields must be complete'
-      redirect_to '/users/new'
+      flash[:errors] = @user.errors.full_messages
+      if @user.errors.details.keys.include?(:email)
+        @user.email = ""
+      end
+      render :new
     end
   end
 
@@ -22,7 +26,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:name, :address, :city, :state, :zip, :email, :password)
+    params.require(:user).permit(:name, :address, :city, :state, :zip, :email, :password)
   end
 
 end
