cbf536ac349a87ae5919bccc4f1ca92c945d5105
Daniel Halverson <daniel.halverson@hotmail.com>
2020-09-08 21:43:49 -0600

    Updates user params password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
+      session[:user_id] = user.id
       redirect_to '/users/profile'
       flash[:success] = "Welcome #{params[:name]}, you are now registered and logged in!"
     else
@@ -14,12 +15,13 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(session[:user_id])
   end
 
   private
 
   def user_params
-    params.permit(:name, :address, :city, :state, :zip, :email, :password_digest)
+    params.permit(:name, :address, :city, :state, :zip, :email, :password)
   end
 
 end
