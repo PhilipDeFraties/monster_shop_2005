8b852e4d23fec996ce9303f851ea6eb0cd49c312
Daniel Halverson <daniel.halverson@hotmail.com>
2020-09-08 19:31:08 -0600

    Add register user route

--- config/routes.rb
+++ config/routes.rb
@@ -33,4 +33,8 @@ Rails.application.routes.draw do
   get "/orders/new", to: "orders#new"
   post "/orders", to: "orders#create"
   get "/orders/:id", to: "orders#show"
+
+  get "/users/new", to: "users#new"
+  post "/users", to: "users#create"
+
 end
