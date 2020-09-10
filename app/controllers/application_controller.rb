ec1aa0da5244b9c1e16915f0bda6657418f5c2e1
Philip DeFraties <philipjfry338@gmail.coom>
2020-09-09 19:57:21 -0600

    Add current_user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,9 @@ class ApplicationController < ActionController::Base
     @cart ||= Cart.new(session[:cart] ||= Hash.new(0))
   end
 
+  helper_method :current_user
+
+  def current_user
+    User.find(session[:user_id])
+  end
 end
