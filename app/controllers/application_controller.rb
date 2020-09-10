58d9c4f874705a1faa5314671e58da3d1ee19908
Philip DeFraties <65036872+PhilipDeFraties@users.noreply.github.com>
2020-09-10 11:40:07 -0600

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :cart
-  helper_method :current_admin?
-  helper_method :current_user
-  helper_method :current_merchant?
+  helper_method :cart,
+                :current_user,
+                :current_admin?,
+                :current_merchant?
 
   def cart
     @cart ||= Cart.new(session[:cart] ||= Hash.new(0))
