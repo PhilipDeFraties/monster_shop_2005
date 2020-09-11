6d93aafae142a5a29e21428652fbb5851730aa64
Philip DeFraties <65036872+PhilipDeFraties@users.noreply.github.com>
2020-09-10 18:54:31 -0600

    Revert "I2 13 user login" (#72)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+
     if @user.save
       session[:user_id] = @user.id
       flash[:success] = "Welcome #{user_params["name"]}, you are now registered and logged in!"
