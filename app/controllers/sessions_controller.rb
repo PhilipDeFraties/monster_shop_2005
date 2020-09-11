b113f137140fd7d8310c8617abef903014aed424
Philip DeFraties <65036872+PhilipDeFraties@users.noreply.github.com>
2020-09-11 15:43:41 -0600

    I2 13 user login (#77)
    * Add user type redirect logic to create action
    
    * Add feature tests for different user logins
    
    Co-authored-by: Philip DeFraties <philipjfry338@gmail.coom>
    Co-authored-by: RyanDBarnett <conner2468@gmail.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,14 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user.authenticate(params[:password])
       session[:user_id] = user.id
-      flash[:success] = "Welcome, #{user.name}!"
-      redirect_to "/users/#{user.id}"
+      flash[:success] = "Welcome, #{user.name}, you are logged in!"
+      if current_merchant?
+        redirect_to "/merchant"
+      elsif current_admin?
+        redirect_to "/admin"
+      else
+        redirect_to "/users/#{user.id}"
+      end
     else
       flash[:error] = "Sorry, your credentials are bad."
       render :new
