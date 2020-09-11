22504c399e42c4adb7d799795d6f441b19512506
Philip DeFraties <65036872+PhilipDeFraties@users.noreply.github.com>
2020-09-10 18:55:43 -0600

    Revert "I1 8 merchant nav restrictions (#67)"
    This reverts commit 76a9388fb707726aff1bf9614102a422838a3231.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/", to: "application#index"
-
+  
   get "/login", to: "sessions#new"
   post '/login', to: 'sessions#create'
 
@@ -46,8 +46,4 @@ Rails.application.routes.draw do
   get "/register", to: "users#new"
   post "/register", to: "users#create"
   get "/users/:id", to: "users#show"
-
-  namespace :admin do
-    get '/', to: 'dashboard#index'
-  end
 end
