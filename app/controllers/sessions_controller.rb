b48380c2817db854f30d9cafdbafc6c16b2cc9c8
Philip DeFraties <philipjfry338@gmail.coom>
2020-09-11 19:11:21 -0600

    Edit user redirect path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
+   def create
     user = User.find_by(email: params[:email])
     if user.authenticate(params[:password])
       session[:user_id] = user.id
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
       elsif current_admin?
         redirect_to "/admin"
       else
-        redirect_to "/users/#{user.id}"
+        redirect_to "/profile"
       end
     else
       flash[:error] = "Sorry, your credentials are bad."
