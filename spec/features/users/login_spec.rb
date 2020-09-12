a833ad65c531b969923a156122bd127103f3130c
Philip DeFraties <philipjfry338@gmail.coom>
2020-09-12 11:33:51 -0600

    Refactor entire file

--- spec/features/users/login_spec.rb
+++ spec/features/users/login_spec.rb
@@ -26,12 +26,12 @@ RSpec.describe "Logging In" do
                         email: 'bill.gates@outlook.com',
                         password: '@%)abc123#$.',
                         role: 1)
-    @admin_1 = User.create(name: 'Bill Gates',
+    @admin_1 = User.create(name: 'Phil DeFraties',
                         address: '1000 Microsoft Drive',
                         city: 'Seattle',
                         state: 'WA',
                         zip: '00123',
-                        email: 'bill.gates@outlook.com',
+                        email: 'Phil.gates@outlook.com',
                         password: '@%)abc123#$.',
                         role: 2)
     end
@@ -50,19 +50,6 @@ RSpec.describe "Logging In" do
     end
   end
 
-  describe "When I log in as a admin with valid credentials" do
-    it "I am redirected to my admin dashboard page" do
-
-      visit "/login"
-
-      fill_in :email, with: @admin_1.email
-      fill_in :password, with: @admin_1.password
-
-      click_on "Log In"
-      expect(current_path).to eq("/admin")
-      expect(page).to have_content("Welcome, #{@admin_1.name}, you are logged in!")
-    end
-  end
 
   describe "When I log in as a merchant with valid credentials" do
     it "I am redirected to my merchant dashboard page" do
@@ -79,6 +66,19 @@ RSpec.describe "Logging In" do
     end
   end
 
+  describe "When I log in as a admin with valid credentials" do
+    it "I am redirected to my admin dashboard page" do
+
+      visit "/login"
+
+      fill_in :email, with: @admin_1.email
+      fill_in :password, with: @admin_1.password
+
+      click_on "Log In"
+      expect(current_path).to eq("/admin")
+      expect(page).to have_content("Welcome, #{@admin_1.name}, you are logged in!")
+    end
+  end
 
   it "I see a failed log in message if I submit an invalid password" do
 
