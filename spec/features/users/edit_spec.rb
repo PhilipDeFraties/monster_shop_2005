fc82142fb82955408e6372c23edfee981a983550
Daniel Lessenden <dless27@gmail.com>
2020-09-12 12:58:02 -0500

    Create test file for edit profile

---
+++ spec/features/users/edit_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "Logging In" do
+  describe "As a registered user" do
+    describe "When I visit my profile page I see a link to edit my profile data" do
+      it "When I click on the link to edit my profile data I see a form like the registration page" do
+
+      end
+    end
+  end
+
+end
