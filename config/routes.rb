64d69b9f9e378f5cdf41261fa9651e8ff66e0634
Philip DeFraties <philipjfry338@gmail.coom>
2020-09-13 17:15:18 -0600

    Edit merchant show page path to use show action

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
 
   #this route is for US4 and the merchant dashboard
   namespace :merchant do
-    get '/', to: 'dashboard#index'
+    get '/', to: 'dashboard#show'
   end
 
   get "/items", to: "items#index"
