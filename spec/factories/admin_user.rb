86ce4bcd09fac710067c3650279ff09e00916e83
RyanDBarnett <conner2468@gmail.com>
2020-09-13 10:02:50 -0600

    Add admin, merchant, item, and item order factories

--- spec/factories/admin_user.rb
+++ spec/factories/admin_user.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
-  factory :admin, class: User do
-    name { 'Jeff Bezos' }
-    address { '123 Main Street' }
+  factory :admin_user, class: User do
+    sequence(:name) { |n| "Merchant Name #{n}" }
+    sequence(:address) { |n| "#{n} Main Street" }
     city { 'Denver' }
     state { 'CO' }
     zip { '80123' }
-    email { 'jbezos@amazon.com' }
-    password { 'Hunter2' }
+    sequence(:email) { |n| "merchantemail#{n}@gmail.com" }
+    sequence(:password) {|n| "Hunter#{n}" }
     role { 2 }
   end
 end
