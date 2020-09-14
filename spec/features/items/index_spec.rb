7bc449df422a0d9832784d7aaa75e4a5676420ac
Philip DeFraties <philipjfry338@gmail.coom>
2020-09-13 21:51:45 -0600

    Add test for most popular items

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -122,30 +122,44 @@ RSpec.describe "Items Index Page" do
       describe "When I visit the items index page" do
         it "I see the top 5 most popular items by quantity purchased, plus the quantity bought" do
 
-          frisbee = @brian.items.create(name: "Frisbee", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:true, inventory: 21)
-          slinky = @brian.items.create(name: "Slinky", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:true, inventory: 21)
-          foot = @brian.items.create(name: "foor", description: "Its a foot", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:true, inventory: 21)
-          paper = @meg.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
-          pencil = @meg.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+          bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+          dog_shop = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+
+          frisbee = bike_shop.items.create(name: "Frisbee", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:true, inventory: 21)
+          slinky = bike_shop.items.create(name: "Slinky", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:true, inventory: 21)
+          foot = bike_shop.items.create(name: "Foot", description: "Its a foot", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:true, inventory: 21)
+          paper = dog_shop.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
+          pencil = dog_shop.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+          pull_toy = dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
 
           order_1 = Order.create!(name: "Phil", address: "5 st", city: "Lakewood", state: "CO", zip: 55555)
           order_2 = Order.create!(name: "Jade", address: "5 st", city: "Lakewood", state: "CO", zip: 55555)
+          order_3 = Order.create!(name: "Endo", address: "5 st", city: "Lakewood", state: "CO", zip: 55555)
+          order_4 = Order.create!(name: "Nooch", address: "5 st", city: "Lakewood", state: "CO", zip: 55555)
 
-          item_order = ItemOrder.create!(order: order_2, item: frisbee, price: 21, quantity: 1)
-          item_order2 = ItemOrder.create!(order: order_1, item: slinky, price: 21, quantity: 2)
-          item_order3 = ItemOrder.create!(order: order_2, item: foot, price: 21, quantity: 3)
-          item_order4 = ItemOrder.create!(order: order_1, item: paper, price: 21, quantity: 4)
-          item_order5 = ItemOrder.create!(order: order_2, item: pencil, price: 21, quantity: 5)
-          item_order6 = ItemOrder.create!(order: order_1, item: @tire, price: 21, quantity: 6)
-          item_order7 = ItemOrder.create!(order: order_1, item: frisbee, price: 21, quantity: 1)
-          item_order8 = ItemOrder.create!(order: order_2, item: slinky, price: 21, quantity: 2)
-          item_order9 = ItemOrder.create!(order: order_1, item: foot, price: 21, quantity: 3)
-          item_order10 = ItemOrder.create!(order: order_2, item: paper, price: 21, quantity: 8)
-          item_order11 = ItemOrder.create!(order: order_2, item: pencil, price: 21, quantity: 5)
-          item_order12 = ItemOrder.create!(order: order_1, item: @dog_bone, price: 21, quantity: 8)
+          item_order = ItemOrder.create!(order: order_1, item: frisbee, price: 21, quantity: 1)
+          item_order2 = ItemOrder.create!(order: order_2, item: slinky, price: 21, quantity: 2)
+          item_order3 = ItemOrder.create!(order: order_3, item: foot, price: 21, quantity: 3)
+          item_order4 = ItemOrder.create!(order: order_4, item: paper, price: 21, quantity: 4)
+          item_order5 = ItemOrder.create!(order: order_1, item: slinky, price: 21, quantity: 2)
+          item_order6 = ItemOrder.create!(order: order_2, item: foot, price: 21, quantity: 3)
+          item_order7 = ItemOrder.create!(order: order_3, item: paper, price: 21, quantity: 4)
+          item_order8 = ItemOrder.create!(order: order_4, item: frisbee, price: 21, quantity: 1)
+          item_order9 = ItemOrder.create!(order: order_2, item: frisbee, price: 21, quantity: 1)
+          item_order10 = ItemOrder.create!(order: order_1, item: pencil, price: 21, quantity: 1)
+          item_order11 = ItemOrder.create!(order: order_2, item: pencil, price: 21, quantity: 1)
+          item_order12 = ItemOrder.create!(order: order_1, item: pull_toy, price: 21, quantity: 1)
+          item_order13 = ItemOrder.create!(order: order_2, item: pull_toy, price: 21, quantity: 1)
 
           visit '/items'
-          save_and_open_page
+
+          within '#most-popular-items' do
+            expect(page).to have_content("Lined Paper: 8")
+            expect(page).to have_content("Foot: 6")
+            expect(page).to have_content("Slinky: 4")
+            expect(page).to have_content("Frisbee: 3")
+            expect(page).to have_content("Pull Toy: 2")
+          end
         end
       end
     end
