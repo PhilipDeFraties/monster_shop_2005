4f543b5bc655b3aef39aa842a24ab3e13f24d199
Philip DeFraties <philipjfry338@gmail.coom>
2020-09-13 21:53:28 -0600

    Add most_popular_item model method

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -9,10 +9,10 @@ class ItemOrder <ApplicationRecord
   end
 
   def self.most_popular_items
-    items_hash = ItemOrder.all.group(:item_id).sum(:quantity).sort_by {|item_id, quantity| [-item_id, quantity]}
+    items_hash = ItemOrder.group(:item_id).sum(:quantity).sort_by {|item_id, quantity| -quantity}
     result = items_hash.map do |item_id, quantity|
       [Item.find(item_id), quantity]
     end
-    binding.pry
+    result[0..4]
   end
 end
