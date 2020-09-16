72acf974b82cad6f9bbeaeb16935a12af6d51ab7
Daniel Lessenden <dless27@gmail.com>
2020-09-16 00:11:22 -0500

    Create merchant column for user, as well as tests for merchant show, as well as the foundation view logic

--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class Merchant::DashboardController < Merchant::BaseController
 
   def show
+    @orders = Order.where(status: :pending).joins(:items).where(items: {merchant_id: current_user.merchant.id})
   end
 
 end
