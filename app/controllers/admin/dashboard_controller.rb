1e9cff568e72394c3de724185969cc6d87045662
Philip DeFraties <philipjfry338@gmail.coom>
2020-09-16 22:59:56 -0600

    Edit merchant_update action

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -9,4 +9,11 @@ class Admin::DashboardController < Admin::BaseController
   def merchants_index
     @merchants = Merchant.all
   end
+
+  def merchant_update
+    merchant = Merchant.find(params[:merchant_id])
+    merchant.update(status: 'Disabled')
+    flash[:success] = "#{merchant.name}'s account is now disabled'"
+    redirect_to '/admin/merchants'
+  end
 end
