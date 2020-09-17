f8b0d5527946086657a9325f09f0609cd54f551b
Daniel Lessenden <dless27@gmail.com>
2020-09-17 14:01:57 -0500

    Delete commented out code

--- app/controllers/admin/merchant_controller.rb
+++ app/controllers/admin/merchant_controller.rb
@@ -2,10 +2,6 @@ class Admin::MerchantController < Admin::BaseController
 
   def show
     @merchant = Merchant.find(params[:merchant_id])
-    # binding.pry
     @orders = Order.where(status: :pending).joins(:items).where(items: {merchant_id: @merchant.id}).distinct
-    # if current_user.role == "admin"
-    #   redirect_to "/admin/merchants/#{@merchant.id}"
-    # end
   end
 end
