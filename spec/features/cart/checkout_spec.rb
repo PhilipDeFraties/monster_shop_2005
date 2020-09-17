23391da9ddcc603f3453e817d249de81d089b325
Philip DeFraties <65036872+PhilipDeFraties@users.noreply.github.com>
2020-09-17 16:06:42 -0600

    Final refactor (#129)
    * Edit login sadpath tests, split into 2 tests
    
    * Add sadpath logic to create action
    
    * Add users/admin spec directory
    
    * Add admin/merchant_index view
    
    * Add admin/merchant_index route
    
    * Add merchant_index action
    
    * Add partial directory
    
    * Add test for checkout sadpath
    
    * Add partial for view
    
    * Add .to_sentence to sad path message
    
    * Delete merchants_index method, moved to admin::merchants_controller
    
    * Update login_spec.rb
    
    delete duplicate lines of code
    
    Co-authored-by: Philip DeFraties <philipjfry338@gmail.coom>
    Co-authored-by: Daniel Lessenden <61212296+D-Lessenden@users.noreply.github.com>

--- spec/features/cart/checkout_spec.rb
+++ spec/features/cart/checkout_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe 'Cart show' do
       click_on "Add To Cart"
       @items_in_cart = [@paper,@tire,@pencil]
     end
-    
+
     it 'Theres a link to checkout' do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
       visit "/cart"
@@ -47,9 +47,24 @@ RSpec.describe 'Cart show' do
       fill_in 'Zip', with: "#{@user_1.zip}"
       click_on "Create Order"
       expect(current_path).to eq("/profile/orders")
+    end
 
+    describe "As a visitor when I click create order on the cart show page" do
+      it "I see a flash message indicating I need to be logged in" do
+        visit "/cart"
+        click_on "Checkout"
+        fill_in 'Name', with: "#{@user_1.name}"
+        fill_in 'Address', with: "#{@user_1.address}"
+        fill_in 'City', with: "#{@user_1.city}"
+        fill_in 'State', with: "#{@user_1.state}"
+        fill_in 'Zip', with: "#{@user_1.zip}"
+        click_on "Create Order"
+        expect(current_path).to eq("/orders")
+        expect(page).to have_content("Must log in before checking out")
+      end
     end
   end
+
 
   describe 'When I havent added items to my cart' do
     it 'There is not a link to checkout' do
