b895f8ffeab1b627a2aceed39a6c334ed260fd0a
Philip DeFraties <philipjfry338@gmail.coom>
2020-09-17 11:31:28 -0600

    Add controller for admin/merchants

---
+++ app/controllers/admin/merchants_controller.rb
@@ -0,0 +1,13 @@
+class Admin::MerchantsController < Admin::BaseController
+
+  def index
+    @merchants = Merchant.all
+  end
+
+  def update
+    merchant = Merchant.find(params[:merchant_id])
+    merchant.update(status: 'Disabled')
+    flash[:success] = "#{merchant.name}'s account is now disabled'"
+    redirect_to '/admin/merchants'
+  end
+end
