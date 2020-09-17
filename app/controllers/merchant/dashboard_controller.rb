77fc00f6ba945b3918c16df47ff091b9d6bd292d
Philip DeFraties <philipjfry338@gmail.coom>
2020-09-17 11:35:37 -0600

    Remove admin/merchant actions from dashboard controller

--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -1,6 +1,10 @@
 class Merchant::DashboardController < Merchant::BaseController
 
   def show
+    #if current_user = merchant, look up merchant via merchant id:
+     @merchant = Merchant.find(current_user.merchant_id)
+    #elsif current_user = amin, look up merchant via params[:merchant_id]
+    # @merchant = Merchant.find(params[:merchant_id])
     @orders = Order.where(status: :pending).joins(:items).where(items: {merchant_id: current_user.merchant.id}).distinct
   end
 
