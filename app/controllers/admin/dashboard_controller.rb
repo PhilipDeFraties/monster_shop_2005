2aad1e917c242ed9ffddb3939da0621d5dc9b554
Philip DeFraties <philipjfry338@gmail.coom>
2020-09-17 11:36:21 -0600

    Remove admin/merchant actions from dashboard controller

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -6,14 +6,4 @@ class Admin::DashboardController < Admin::BaseController
   def index
   end
 
-  def merchants_index
-    @merchants = Merchant.all
-  end
-
-  def merchant_update
-    merchant = Merchant.find(params[:merchant_id])
-    merchant.update(status: 'Disabled')
-    flash[:success] = "#{merchant.name}'s account is now disabled'"
-    redirect_to '/admin/merchants'
-  end
 end
