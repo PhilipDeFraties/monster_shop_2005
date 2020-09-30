af2f6b96d1e34e5be3b97d7dfd92048ecefb54f4
Daniel Lessenden <dless27@gmail.com>
2020-09-30 00:12:07 -0500

    Update routes with resources and namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -7,34 +7,44 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  get "/merchants", to: "merchants#index"
-  get "/merchants/new", to: "merchants#new"
-  get "/merchants/:id", to: "merchants#show"
-  post "/merchants", to: "merchants#create"
-  get "/merchants/:id/edit", to: "merchants#edit"
-  patch "/merchants/:id", to: "merchants#update"
-  delete "/merchants/:id", to: "merchants#destroy"
-
+  resources :merchants
+  # get "/merchants", to: "merchants#index"
+  # get "/merchants/new", to: "merchants#new"
+  # get "/merchants/:id", to: "merchants#show"
+  # post "/merchants", to: "merchants#create"
+  # get "/merchants/:id/edit", to: "merchants#edit"
+  # patch "/merchants/:id", to: "merchants#update"
+  # delete "/merchants/:id", to: "merchants#destroy"
   namespace :merchant do
     get '/', to: 'dashboard#show'
     get '/items', to: 'dashboard#index'
   end
 
-  get "/items", to: "items#index"
-  get "/items/:id", to: "items#show"
-  get "/items/:id/edit", to: "items#edit"
-  patch "/items/:id", to: "items#update"
-  get "/merchants/:merchant_id/items", to: "items#index"
-  get "/merchants/:merchant_id/items/new", to: "items#new"
-  post "/merchants/:merchant_id/items", to: "items#create"
-  delete "/items/:id", to: "items#destroy"
+  resources :items, :except => [:new, :create]
+  # get "/items", to: "items#index"
+  # get "/items/:id", to: "items#show"
+  # get "/items/:id/edit", to: "items#edit"
+  # patch "/items/:id", to: "items#update"
+  # delete "/items/:id", to: "items#destroy"
 
   get "/items/:item_id/reviews/new", to: "reviews#new"
   post "/items/:item_id/reviews", to: "reviews#create"
+  # namespace :items do
+  #   get "/:item_id/reviews/new", to: "reviews#new"
+  #   post "/:item_id/reviews", to: "reviews#create"
+  # end
 
-  get "/reviews/:id/edit", to: "reviews#edit"
-  patch "/reviews/:id", to: "reviews#update"
-  delete "/reviews/:id", to: "reviews#destroy"
+  # scope :merchants do
+  #   resources :items, :only => [:index, :new, :create]
+  # end
+  get "/merchants/:merchant_id/items", to: "items#index"
+  get "/merchants/:merchant_id/items/new", to: "items#new"
+  post "/merchants/:merchant_id/items", to: "items#create"
+
+  resources :reviews, :only => [:edit, :update, :destroy]
+  # get "/reviews/:id/edit", to: "reviews#edit"
+  # patch "/reviews/:id", to: "reviews#update"
+  # delete "/reviews/:id", to: "reviews#destroy"
 
   post "/cart/:item_id", to: "cart#add_item"
   get "/cart", to: "cart#show"
@@ -43,12 +53,18 @@ Rails.application.routes.draw do
   patch "/cart/:item_id", to: "cart#update"
   patch "/cart/:item_id/decrease", to: "cart#decrease"
 
-  get "/orders/new", to: "orders#new"
-  post "/orders", to: "orders#create"
+  resources :orders, :only => [:new, :create]
 
-  get "/profile/orders", to: "orders#index"
-  get "/profile/orders/:id", to: "orders#show"
-  delete "profile/orders/:id", to: "orders#destroy"
+  # get "/orders/new", to: "orders#new"
+  # post "/orders", to: "orders#create"
+
+  scope :profile do
+    resources :orders, :only => [:index, :show, :destroy]
+  end
+  # get "/profile/orders", to: "orders#index"
+  # get "/profile/orders/:id", to: "orders#show"
+  # delete "profile/orders/:id", to: "orders#destroy"
+
 
   get "/profile", to: "users#show"
   get "/profile/edit", to: "users#edit"
